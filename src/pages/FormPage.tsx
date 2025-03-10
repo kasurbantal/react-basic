21d19cf7a15eb499c22ec27a0e62edab9621e0e5
kasurbantal <kasurbantal880@gmail.com>
2025-03-10 21:47:52 +0700

    feat: adding email state

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,15 +1,40 @@
+import { useRef, useState } from "react";
+
 const FormPage = () => {
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const inputEmailRef = useRef<HTMLInputElement>(null);
+
+  const [fullNameInput, setfullNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+
   const handleSubmit = () => {
-    alert("Form Submited");
+    const fullNameForValue = inputNameRef.current?.value;
+    const emailForValue = inputEmailRef.current?.value;
+    alert("Form Submited " + fullNameForValue + " " + emailForValue);
   };
 
   return (
     <>
       <h1>Form Page</h1>
 
+      <h3>{fullNameInput}</h3>
+      <h3>{emailInput}</h3>
+
       <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
         <label htmlFor="full-name">Full Name</label>
-        <input id="full-name" type="text" />
+        {/* <input ref={inputNameRef} id="full-name" type="text" /> */}
+        <input
+          onChange={(e) => setfullNameInput(e.target.value)}
+          id="full-name"
+          type="text"
+        />
+        <label htmlFor="email">Email</label>
+        {/* <input ref={inputEmailRef} id="email" type="email" /> */}
+        <input
+          onChange={(e) => setEmailInput(e.target.value)}
+          id="email"
+          type="email"
+        />
         <button onClick={handleSubmit}>Submit</button>
       </div>
     </>
