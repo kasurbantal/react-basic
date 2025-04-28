c2d1c57065224affb8dcc234803fe1f19ae8a687
kasurbantal <kasurbantal880@gmail.com>
2025-04-29 06:33:47 +0700

    feat: add data in mock-db.json and table in EmployeesPage

--- src/pages/EmployeesPage.tsx
+++ src/pages/EmployeesPage.tsx
@@ -2,6 +2,20 @@ const EmployeesPage = () => {
   return (
     <>
       <h1>Employees Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Luwak</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
